48080a7b2cbd25a1fc826300b04fccddb48ee38d
KoRrNiK <koliberr123456789@gmail.com>
2022-02-22 12:51:22 +0100

    f ix

--- js/script.js
+++ js/script.js
@@ -47,14 +47,14 @@ function decryptStart() {
 
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-	let word = $("#TextToDecrypt").val();
+	let word = $("#codeText").val();
 	let firstnumber = Number($("#firstnumber").val());
 	let secondnumber = Number($("#secondnumber").val());
 	let shift = number_First_Second(firstnumber, secondnumber);
 	let decyrptedCode = "";
 
-	const lengthA = alphabet.length;
-	const lengthW = word.length;
+	let lengthA = alphabet.length;
+	let lengthW = word.length;
 
 	for (var i = 0; i < lengthW; i++) {
 		for (var x = 0; x < lengthA; x++) {
@@ -72,12 +72,12 @@ function decryptStart() {
 
 
 	if (lengthW > 0) {
+		
 		$(".codeDecrypte").show();
+		$("#decyrptedCode").html(decyrptedCode);
+		copyText(500);
+
 	} else $(".codeDecrypte").hide();
-
-	$("#decyrptedCode").html(decyrptedCode);
-
-	copyText(500);
 
 }
 
